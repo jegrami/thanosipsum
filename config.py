0a66c0ab9c508974e2e81ad415a5e0e537bec8f3
jeremiah igrami <jegrami.dev@gmail.com>
2025-03-22 14:38:06 +0100

    modify config for deployment on render

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,9 +8,21 @@ basedir = Path(__file__).parent.resolve()
 connexion_app = connexion.FlaskApp(__name__, specification_dir=basedir)
 app = connexion_app.app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'thanos3.db'}"
+
+# get db uri from Render
+database_url = os.environ.get("DATABASE_URL", f"sqlite:///{basedir / 'thanos3.db'}")
+
+# Fix potential Postgres URL format issue (Render uses postgres:// but SQLAlchemy needs postgresql://)
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+
+# Configure the database
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["DEBUG"] = True
+
+
+app.config["DEBUG"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
